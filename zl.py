29200b0821483d9413938132c43f9f8cb999c5ba
Xiao Chen <xiao@cloudera.com>
2015-10-24 22:37:58 -0700

    Squeezed into 100 lines.

--- zl.py
+++ zl.py
@@ -1,95 +1,77 @@
 from decimal import *
-import pdb
-import sys, getopt
+import getopt, pdb, sys
 
 g_result = []
 g_buckets = "N/A"
 g_step = "N/A"
 g_outfile = None
-#g_sum = 1
 
 def main():
+  global g_buckets, g_step, g_outfile
   outName = "results.csv"
   opt, args = getopt.getopt(sys.argv[1:], "n:s:o:")
-#  pdb.set_trace()
   try:
     for option, arg in opt:
       if option == '-n':
-        global g_buckets
         g_buckets = int (arg)
       elif option == '-s':
-        global g_step
         g_step = Decimal (arg)
       elif option == '-o':
         outName = arg + ".csv"
-        print("Result will be saved to", outName)
-      else:
-        printUsage()
   except ValueError:
-    printUsage()
-    exit()
+    printUsageAndExit()
   
   if g_buckets == "N/A" or g_step == "N/A":
-      printUsage()
-      exit()
+      printUsageAndExit()
  
-  if (validateInput() is not True):
+  if validateInput() is not True:
     exit()
  
-#  global g_sum
-#  calcsum = int(log10(g_step))+1
-#  g_step *= 10 ^ calcsum
-#  g_sum *= 10 ^ calcsum
+  print("Calculating with buckets=%s and step=%s" % (g_buckets, g_step))
+  print("Result will be saved to %s" % outName)
   try:  
-    global g_outfile
     g_outfile = open(outName, "w")
     drawZL()
-  
     saveFile()
     g_outfile.close()
   except KeyboardInterrupt:
     print("\nStopped")
+    g_outfile.close()
     exit()
   except:
-    if g_outfile is not None:
-      g_outfile.close()
+    g_outfile.close()
     print("\nFailed with unknown error.")
     raise
   print("\nCompleted")
 
-def getBase():
-  return [Decimal(0.0)] * g_buckets
-
 def drawZL():
   leftsum = Decimal(1.0)
   pos = 0
-#  pdb.set_trace()
-  base = getBase()
+  base = [Decimal(0.0)] * g_buckets
   drawRest(base, leftsum, pos)
 
 def drawRest(base, leftsum, pos):
   global g_result
   if leftsum == 0.0:
     g_result.append(base)
-#    print("added %s", base)
+#  print("added %s", base)
     return
 
-  res = base
   if pos == g_buckets:
-   return 
-  res[pos] = leftsum
-  g_result.append(res[:])
+    return
+  base[pos] = leftsum
+  g_result.append(base[:])
   if (len(g_result) > 100):
     saveFile()
     sys.stdout.write(".")
     sys.stdout.flush()
     del g_result[:]
-#  print("added %s", res)
+#  print("added %s", base)
 
   curleftsum = leftsum - g_step
   while (curleftsum >= 0):
-    res[pos] = curleftsum
-    drawRest(res, leftsum - curleftsum, pos + 1)
+    base[pos] = curleftsum
+    drawRest(base, leftsum - curleftsum, pos + 1)
     curleftsum -= g_step
 
 def validateInput():
@@ -109,12 +91,10 @@ def validateInput():
 
 def saveFile():
   for row in g_result:
-   g_outfile.write(', '.join(str(x) for x in row) + '\n')
+    g_outfile.write(', '.join(str(x) for x in row) + '\n')
 
-def printUsage():
+def printUsageAndExit():
   print("Usage:\n     %s [-n] number_of_buskets [-s] step_size [-o] output_file_name" % sys.argv[0])
-
-
-
+  exit()
 
 main()
